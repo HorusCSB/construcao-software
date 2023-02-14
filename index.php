9ae075f03a3a278505bfe579c36236ac4aaa5dd4
horus <horus.cristian@gmail.com>
2023-02-13 21:34:20 -0300

    fix: fix header pages

--- index.php
+++ index.php
@@ -42,43 +42,13 @@
                     <td>
                         <table id="cab">
                             <tr>
-                                <td width="60px"></td>
-                                <td width="60px"></td>
-                                <td width="60px"></td>
-                                <td width="60px"></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
                             </tr>
                             <tr>
                                 <td>
-                                    <select id="estado" name="estado">
-                                        <option value="">Estado</option>
-                                        <option value="ac">Acre</option>
-                                        <option value="al">Alagoas</option>
-                                        <option value="am">Amazonas</option>
-                                        <option value="ap">Amapá</option>
-                                        <option value="ba">Bahia</option>
-                                        <option value="ce">Ceará</option>
-                                        <option value="df">Distrito Federal</option>
-                                        <option value="es">Espírito Santo</option>
-                                        <option value="go">Goiás</option>
-                                        <option value="ma">Maranhão</option>
-                                        <option value="mt">Mato Grosso</option>
-                                        <option value="ms">Mato Grosso do Sul</option>
-                                        <option value="mg">Minas Gerais</option>
-                                        <option value="pa">Pará</option>
-                                        <option value="pb">Paraíba</option>
-                                        <option value="pr">Paraná</option>
-                                        <option value="pe">Pernambuco</option>
-                                        <option value="pi">Piauí</option>
-                                        <option value="rj">Rio de Janeiro</option>
-                                        <option value="rn">Rio Grande do Norte</option>
-                                        <option value="ro">Rondônia</option>
-                                        <option value="rs">Rio Grande do Sul</option>
-                                        <option value="rr">Roraima</option>
-                                        <option value="sc">Santa Catarina</option>
-                                        <option value="se">Sergipe</option>
-                                        <option value="sp">São Paulo</option>
-                                        <option value="to">Tocantins</option>
-                                    </select>
                                 </td>
                                 <td id="nav-create-account" class="nav-click" onclick="loadPage('create-account-page')">
                                     Crie sua Conta</td>
